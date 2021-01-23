b5c5b1a00fbf98d2e5db3d2f5aa3d9323b6b37c1
rhysemmas <rhys.emmas@uswitch.com>
2021-01-23 17:05:48 +0000

    Init

---
+++ server.cpp
@@ -0,0 +1,61 @@
+// rf95_server.pde
+// -*- mode: C++ -*-
+// Example sketch showing how to create a simple messageing server
+// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
+// reliability, so you should only use RH_RF95  if you do not need the higher
+// level messaging abilities.
+// It is designed to work with the other example rf95_client
+// Tested with Anarduino MiniWirelessLoRa, Rocket Scream Mini Ultra Pro with
+// the RFM95W, Adafruit Feather M0 with RFM95
+
+#include <SPI.h>
+#include <RH_RF95.h>
+
+// Singleton instance of the radio driver
+RH_RF95 rf95;
+
+int led = LED_BUILTIN;
+float frequency = 868.1;
+
+void setup() 
+{ 
+  Serial.begin(9600);
+  while (!Serial); // Wait for serial port to be available
+    if (!rf95.init())
+      Serial.println("init failed");
+
+    pinMode(led, OUTPUT);
+    rf95.setFrequency(frequency);
+    rf95.setModemConfig(RH_RF95::Bw125Cr45Sf2048);
+    rf95.setTxPower(20, false);
+}
+
+void loop()
+{
+  if (rf95.available())
+  {
+    // Should be a message for us now   
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+    if (rf95.recv(buf, &len))
+    {
+      digitalWrite(led, HIGH);
+//      RH_RF95::printBuffer("request: ", buf, len);
+      Serial.print("got request: ");
+      Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);
+      
+      // Send a reply
+      uint8_t data[] = "And hello back to you";
+      rf95.send(data, sizeof(data));
+      rf95.waitPacketSent();
+      Serial.println("Sent a reply");
+       digitalWrite(led, LOW);
+    }
+    else
+    {
+      Serial.println("recv failed");
+    }
+  }
+}
