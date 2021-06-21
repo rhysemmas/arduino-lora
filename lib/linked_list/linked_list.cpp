7dcf8d6d0facb2e42b3e4cedf79ade520e9ea60e
rhysemmas <rhys.emmas@ntlworld.com>
2021-06-21 22:27:32 +0100

    Use linked lists instead of queues

---
+++ lib/linked_list/linked_list.cpp
@@ -0,0 +1,64 @@
+#include "linked_list.h"
+
+linked_list::linked_list() {
+  head = NULL;
+  tail = NULL;
+  count = 0;
+}
+
+linked_list::~linked_list() {
+  delete head;
+  delete tail;
+}
+
+void linked_list::add_node(Packet pkt) {
+  node *new_node = new node;
+  new_node->packet = pkt;
+  new_node->prev = NULL;
+  new_node->next = NULL;
+
+  if (head == NULL) {
+    head = new_node;
+    tail = new_node;
+
+    count++;
+    return;
+  }
+
+  new_node->prev = tail;
+  tail->next = new_node;
+  tail = new_node;
+  count++;
+}
+
+void linked_list::delete_node(Packet pkt) {
+  node *del_node;
+  del_node = head;
+
+  while(del_node->next != nullptr) {
+    // messages need IDs or Packet needs isEqual func/operator
+    if (del_node->packet.data.message == pkt.data.message) {
+      node *prev_node;
+      prev_node = del_node->prev;
+      prev_node->next = del_node->next;
+      delete del_node;
+
+      count--;
+      return;
+    }
+
+    del_node = del_node->next;
+  }
+}
+
+int linked_list::size() {
+  return count;
+}
+
+bool linked_list::is_empty() {
+  return count == 0;
+}
+
+Packet linked_list::peek() {
+  return head->packet;
+}
