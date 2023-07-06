acd883a629a957220d4b7976118df99c9e8c0bfa
rhysemmas <rhys.emmas@ntlworld.com>
2023-07-07 00:35:41 +0100

    Wait for serial to be available

--- src/main.cpp
+++ src/main.cpp
@@ -34,9 +34,12 @@ void setup() {
   Serial.println();
   uint8_t buffer[1] = {0};
   size_t bufSize = 0;
-  bufSize = Serial.readBytesUntil('\n', buffer, sizeof(buffer));
-  if (bufSize > 0) {
-    rf95.setHeaderFrom(buffer[0]);
+
+  if (Serial.available() > 0) {
+    bufSize = Serial.readBytesUntil('\n', buffer, sizeof(buffer));
+    if (bufSize > 0) {
+      rf95.setHeaderFrom(buffer[0]);
+    }
   }
 
   Serial.print(F("max message length: "));
@@ -137,9 +140,13 @@ void loop() {
 
     uint8_t buf[1] = {0};
     size_t bufS = 0;
-    bufS = Serial.readBytesUntil('\n', buf, sizeof(buf));
-    if (bufS > 0) {
-      rf95.setHeaderTo(buf[0]);
+    if (Serial.available() > 0) {
+      bufS = Serial.readBytesUntil('\n', buf, sizeof(buf));
+      if (bufS > 0) {
+        rf95.setHeaderTo(buf[0]);
+        Serial.print(F("sending message to: "));
+        Serial.println(buf[0]);
+      }
     }
     send(buffer, bufSize);
     waitForReply();
